bb2441aa21540d4cdb744f3c237e36f996b0e267
CafeManager <kevinflores1997@yahoo.com>
2023-08-16 11:26:41 -0700

    starter code

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+# from models import db, connect_db, User
+from flask_sqlalchemy import SQLAlchemy
+from form import AddUserForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this-is-secr3t'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///sharecipe'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+# connect_db(app)
+
+@app.route("/")
+def home_page():
+    return render_template('homepage.html')
---
+++ models.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_bcrypt import Bcrypt
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default="/static/images/default-pic.png",
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}, {self.email}>"
+
+    
+    @classmethod
+    def signup(cls, username, email, password, image_url):
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            image_url=image_url,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+    
